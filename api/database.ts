694e14597b940025b6b732679ecf560699ee73a7
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2025-02-26 14:15:59 +0000

    frontend url from localstorage

--- api/database.ts
+++ api/database.ts
@@ -19,7 +19,10 @@ export enum CAUSE {
 }
 
 export function getFrontendURL(): string {
-    return 'https://play.2.thinkmay.net';
+    const address = localStorage.getItem('thinkmay_domain');
+    if (address == null) 
+        return 'https://play.2.thinkmay.net'
+    else return `https://${address}`
 }
 export const POCKETBASE = new PocketBase(getFrontendURL());
 export const GLOBAL = () =>
