48913607e3d3d5965b2dfbd27ce9bf1fe902a2cc
pigeatgarlic <huyhoangdo0205@gmail.com>
2025-04-22 02:27:25 +0000

    change global db port

--- api/database.ts
+++ api/database.ts
@@ -26,7 +26,7 @@ export function getFrontendURL(): string {
 export const POCKETBASE = () => new PocketBase(getFrontendURL());
 export const GLOBAL = () =>
     createClient(
-        'https://play.2.thinkmay.net:4432',
+        'https://play.2.thinkmay.net:444',
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE'
     );
 
