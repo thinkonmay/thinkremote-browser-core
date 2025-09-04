00e59af69494d29039b37b4b58f6515ff2fdd17f
pigeatgarlic <huyhoangdo0205@gmail.com>
2025-09-04 05:32:21 +0000

    update supabase self-host

--- api/database.ts
+++ api/database.ts
@@ -31,10 +31,9 @@ export function getFrontendURL(): string {
 export const POCKETBASE = () => new PocketBase(getFrontendURL());
 export const GLOBAL = () =>
     createClient(
-        'https://jsnsuyxuvdstcxskzjsg.supabase.co',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzbnN1eXh1dmRzdGN4c2t6anNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1Mzc0OTUsImV4cCI6MjA1NjExMzQ5NX0.2p5UZ6AFlgm8qv2EjPVw7AYgzOLlN2biXkfHAq4ZhR4'
+        'https://play.2.thinkmay.net:945',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE'
     );
-
 let id = 'unknown';
 const stack: { content: any; timestamp: string }[] = [];
 const value = {
@@ -48,13 +47,13 @@ const value = {
 
 export let DevEnv = false
 if (typeof window != 'undefined') {
-    value.os= getOS()
-    value.browser= getBrowser()
-    value.resolution= getResolution()
-    value.url= window.location.href
-    
+    value.os = getOS()
+    value.browser = getBrowser()
+    value.resolution = getResolution()
+    value.url = window.location.href
+
     DevEnv = window.location.href.includes('localhost') ||
-    ValidateIPaddress(window.location.host.split(':')[0]);
+        ValidateIPaddress(window.location.host.split(':')[0]);
 }
 
 let current_stack_length = 0;
@@ -74,7 +73,7 @@ export async function UserSession(email: string) {
             value.ip = (
                 await (await fetch('https://icanhazip.com/')).text()
             ).replaceAll('\n', '');
-    } catch {}
+    } catch { }
 
     const session = await (async () => {
         if (id != 'unknown') return id;
