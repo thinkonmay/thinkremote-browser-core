1b0fd9881b338cd040487c600c3adf93fb102d46
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2025-09-08 08:59:16 +0700

    avoid assign null

--- singleton/index.ts
+++ singleton/index.ts
@@ -22,10 +22,9 @@ export const Assign = (client: RemoteDesktopClient) => {
 export const ready = async (): Promise<Error | void> => {
     const now = () => new Date().getTime() / 1000;
     const start = now();
-    while (CLIENT == undefined || !CLIENT.Ready()) {
+    while (NotReady()) {
         await new Promise((r) => setTimeout(r, 1000));
         if (now() - start > 10 * 60) return new Error('connect timeout');
-        else if (CLIENT == null) return new Error('null client');
         else if (CLIENT.AuthFailed())
             return new Error('streaming auth failure');
     }
