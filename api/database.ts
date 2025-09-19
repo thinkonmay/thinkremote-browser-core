49789e42157853fc69afece4211802b17b70a35e
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2025-09-19 11:59:59 +0700

    update global domain

--- api/database.ts
+++ api/database.ts
@@ -24,7 +24,7 @@ function getFrontendURL(): string {
 export const POCKETBASE = () => new PocketBase(getFrontendURL());
 export const GLOBAL = () =>
     createClient(
-        'https://play.2.thinkmay.net:445',
+        'https://saigon2.thinkmay.net:445',
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzU0OTMxNjAwLCJleHAiOjE5MTI2OTgwMDB9.m7qcf4j3u1oPoqIsCqU3JHqYEO0DV2PmoPXGcdUAdR8'
     );
 
